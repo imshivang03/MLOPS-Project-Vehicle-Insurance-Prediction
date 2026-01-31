e5171125dbf2b7076484a081c9325973ab88dad0
Shivang <shivang.cse.nitnagaland@gmail.com>
2026-01-31 11:54:16 +0530

    Project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shivang Shahi",
+    author_email="shivang.cse.nitnagaland@gmail.com",
+    packages=find_packages()
+)
